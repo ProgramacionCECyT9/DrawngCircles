11f95bd5894e47656eb3287915da252c887ea3aa
Mauricio <mauriciodinki@gmail.com>
2015-05-09 20:42:32 -0500

    Added Function to draw circles

--- app/src/main/java/com/ipn/mauriciodinki/drawingcircles/MainActivity.java
+++ app/src/main/java/com/ipn/mauriciodinki/drawingcircles/MainActivity.java
@@ -1,9 +1,18 @@
 package com.ipn.mauriciodinki.drawingcircles;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,7 +20,57 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        DrawingView customView = new DrawingView(this);
+        setContentView(customView);
+    }
+
+    class DrawingView extends View {
+        Path path = new Path();
+        float x = -100;
+        float y = -100;
+        String text = "Event";
+        String action = "Action";
+        int radius = 20;
+
+        public DrawingView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            Paint paint = new Paint();
+            canvas.drawColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(6);
+            paint.setColor(Color.WHITE);
+            path.addCircle(x, y, radius, Path.Direction.CW);
+            canvas.drawPath(path, paint);
+
+            paint.setColor(Color.GRAY);
+            paint.setTextSize(20);
+            paint.setStrokeWidth(2);
+            canvas.drawText(text, 300, 25, paint);
+            canvas.drawText("x = " + x +  "  y = " + y, 5, 25, paint);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            x = event.getX();
+            y = event.getY();
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                action = "down";
+                text = "Action Down";
+            }
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                text = "Action Up";
+            }
+            if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                action = "move";
+                text = "Action Move";
+            }
+            invalidate();
+            return true;
+        }
     }
 
 
